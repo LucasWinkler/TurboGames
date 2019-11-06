c60b9595c4ce2d167011f389c607cce59ba70483
Lucas <lucasj.winkler1999@gmail.com>
2019-11-05 23:07:13 -0500

    Changed identity directory naming

---
+++ GameStore/Pages/Account/Settings/Address.cshtml.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using GameStore.Data;
+using GameStore.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+namespace GameStore.Pages.Account.Settings
+{
+    public class AddressModel : PageModel
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly ApplicationDbContext _context;
+
+        public AddressModel(
+            UserManager<ApplicationUser> userManager,
+            SignInManager<ApplicationUser> signInManager,
+            ApplicationDbContext context)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _context = context;
+        }
+
+        [BindProperty]
+        public Address Address { get; set; }
+
+        [TempData]
+        public string StatusMessage { get; set; }
+
+        public async Task<IActionResult> OnGetAsync()
+        {
+            var user = await _userManager.GetUserAsync(User);
+            if (user == null)
+            {
+                return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
+            }
+
+            if (user.AddressId != null)
+            {
+                var address = await _context.Addresses.FirstOrDefaultAsync(x => x.Id == user.AddressId);
+
+                Address = new Address
+                {
+                    PrimaryAddress = address.PrimaryAddress,
+                    SecondaryAddress = address.SecondaryAddress,
+                    City = address.City,
+                    Country = address.Country,
+                    Province = address.Province,
+                    PostalCode = address.PostalCode
+                };
+            }
+
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                ModelState.AddModelError("", "Unable to save.");
+                return Page();
+            }
+
+            var user = await _userManager.GetUserAsync(User);
+            if (user == null)
+            {
+                return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
+            }
+
+            try
+            {
+                _context.Addresses.Add(Address);
+
+                user.AddressId = Address.Id;
+
+                await _context.SaveChangesAsync();
+
+                await _userManager.UpdateAsync(user);
+                await _signInManager.RefreshSignInAsync(user);
+
+                StatusMessage = "Your billing address has been updated";
+                return RedirectToPage();
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine(e.InnerException);
+            }
+
+            return Page();
+        }
+    }
+}
---
+++ GameStore/Pages/Account/Settings/ManageNavPages.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace GameStore.Pages.Account.Settings
+{
+    public static class ManageNavPages
+    {
+        public static string Index => "Index";
+
+        public static string Public => "Public";
+
+        public static string Address => "Address";
+
+        public static string Payment => "Payment";
+
+        public static string ChangePassword => "ChangePassword";
+
+        public static string IndexNavClass(ViewContext viewContext) => PageNavClass(viewContext, Index);
+        
+        public static string PublicNavClass(ViewContext viewContext) => PageNavClass(viewContext, Public);
+
+        public static string AddressNavClass(ViewContext viewContext) => PageNavClass(viewContext, Address);
+        
+        public static string PaymentNavClass(ViewContext viewContext) => PageNavClass(viewContext, Payment);
+
+        public static string ChangePasswordNavClass(ViewContext viewContext) => PageNavClass(viewContext, ChangePassword);
+
+        private static string PageNavClass(ViewContext viewContext, string page)
+        {
+            var activePage = viewContext.ViewData["ActivePage"] as string
+                ?? System.IO.Path.GetFileNameWithoutExtension(viewContext.ActionDescriptor.DisplayName);
+            return string.Equals(activePage, page, StringComparison.OrdinalIgnoreCase) ? "active" : null;
+        }
+    }
+}
