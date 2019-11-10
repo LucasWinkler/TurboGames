8dc22fd04071f7cf2a6201a83bbaa4dfa97b715f
Lucas <lucasj.winkler1999@gmail.com>
2019-11-10 05:17:30 -0500

    Implemented verification emails when changing email

--- GameStore/Pages/Account/Settings/Index.cshtml.cs
+++ GameStore/Pages/Account/Settings/Index.cshtml.cs
@@ -1,7 +1,9 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Encodings.Web;
 using System.Threading.Tasks;
 using GameStore.Data;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -11,17 +13,22 @@ namespace GameStore.Pages.Account.Settings
     {
         private readonly UserManager<ApplicationUser> _userManager;
         private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly IEmailSender _emailSender;
 
         public IndexModel(
             UserManager<ApplicationUser> userManager,
-            SignInManager<ApplicationUser> signInManager)
+            SignInManager<ApplicationUser> signInManager,
+         IEmailSender emailSender)
         {
             _userManager = userManager;
             _signInManager = signInManager;
+            _emailSender = emailSender;
         }
 
         [TempData]
         public string StatusMessage { get; set; }
+
+        public bool IsEmailConfirmed { get; set; }
 
         [BindProperty]
         public InputModel Input { get; set; }
@@ -59,6 +66,8 @@ namespace GameStore.Pages.Account.Settings
                 UserName = username,
                 ShouldReceiveEmails = user.ShouldReceiveEmails
             };
+
+            IsEmailConfirmed = await _userManager.IsEmailConfirmedAsync(user);
 
             return Page();
         }
@@ -113,6 +122,37 @@ namespace GameStore.Pages.Account.Settings
 
             await _signInManager.RefreshSignInAsync(user);
             StatusMessage = "Your account has been updated";
+            return RedirectToPage();
+        }
+
+        public async Task<IActionResult> OnPostSendVerificationEmailAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            var user = await _userManager.GetUserAsync(User);
+            if (user == null)
+            {
+                return NotFound($"Unable to load user with ID '{_userManager.GetUserId(User)}'.");
+            }
+
+
+            var userId = await _userManager.GetUserIdAsync(user);
+            var email = await _userManager.GetEmailAsync(user);
+            var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+            var callbackUrl = Url.Page(
+                "/Account/ConfirmEmail",
+                pageHandler: null,
+                values: new { userId = userId, code = code },
+                protocol: Request.Scheme);
+            await _emailSender.SendEmailAsync(
+                email,
+                "Confirm your email",
+                $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
+
+            StatusMessage = "Verification email sent. Please check your email.";
             return RedirectToPage();
         }
     }
