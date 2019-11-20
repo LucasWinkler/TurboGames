c04467c348c9ec868ac461276ddbd6fb288c2725
Lucas <lucasj.winkler1999@gmail.com>
2019-11-19 23:23:51 -0500

    Moved a property in the Page class

--- GameStore.AutomatedUITests/PageObjects/Page.cs
+++ GameStore.AutomatedUITests/PageObjects/Page.cs
@@ -29,14 +29,14 @@ namespace GameStore.AutomatedUITests.PageObjects
         /// <summary> Gets url for the page that is being tested. </summary>
         private string Url => $"{_baseUrl}:{_port}/{_pageRoute}";
 
+        /// <summary> Gets the web driver that is being used for the page. </summary>
+        protected IWebDriver Driver { get; }
+
         /// <summary> Gets the title of the current page. </summary>
         public string Title => Driver.Title;
 
         /// <summary> Gets the source code for the current page. </summary>
         public string Source => Driver.PageSource;
-
-        /// <summary> Gets the web driver that is being used for the page. </summary>
-        protected IWebDriver Driver { get; }
 
         /// <summary> Uses the web drivers Navigate() method to go to the pages url.  </summary>
         public void Navigate() => Driver.Navigate().GoToUrl(Url);
